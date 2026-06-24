99f127e58e21193e9a135b49c33cc8672d8b1a6f
sabarishwaran261082-SCE <sabarishwaran8229@gmail.com>
2026-06-24 14:19:03 +0530

    Phase 1 completed

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/health")
+def health():
+    return {
+        "status": "UP",
+        "application": "Autonomous DevSecOps Framework"
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
